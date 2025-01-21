1468a61c31713cb8996948952f6f3a7f0aab799b
wpw197 <160235259+wpw197@users.noreply.github.com>
2025-01-21 16:34:00 +0000

    updated

--- analysis script.R
+++ analysis script.R
@@ -3,9 +3,9 @@
 ##
 ## Purpose of script: 
 ##
-## Author: Emily Lane
+## Author: 
 ##
-## Date Created: 
+## Date Created: 21.01.25
 ##
 ## ------------------------------------------------------------------------
 ##
@@ -15,7 +15,7 @@
 ## ------------------------------------------------------------------------
 ## load up the packages we will need: 
 
-packages <- c("tidyverse", "versions", "pmsampsize")
+packages <- c("tidyverse", "versions", "pmsampsize", "car")
 install.packages(setdiff(packages, rownames(installed.packages())))
 lapply(packages, library, character.only = TRUE, quietly = TRUE)  
 rm(packages)
@@ -23,5 +23,14 @@ rm(packages)
 ## ------------------------------------------------------------------------
 # pmsampsize(type = "b", cstatistic = 0.8, parameters = 4, prevalence = 0.197)
 pmsampsize::pmsampsize(type="b",cstatistic = 0.8,parameters = 4,prevalence = 0.197)
+
+# Checking assumptions ----------------------------------------------------
+## age gender smoking subtype 
+## checking for multi collinearity
+## selecting relevant columns 
+collinearity_check <- TRACEx_pts_df %>% 
+  select(age, sex, ethnicity, cigs_perday, years_smoking, packyears,
+         smoking_status_merged, is.family.lung, histology_multi_full_genomically.confirmed)
+
 
 
